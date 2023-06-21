b40cf88b457527d98e5ccd28af8521a85fb296c5
zhengxinchang <zhengxinchang1993@163.com>
2023-06-21 16:06:27 -0500

    reformat code structure

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,15 @@ use rust_htslib::{
 use std::{
     cmp::Ordering,
     collections::HashMap,
-    env,
     fs::File,
-    io::{self, BufWriter, Write},
-    path::{Path, PathBuf},
+    io::{BufWriter, Write},
+    path::{PathBuf},
     process::exit,
 };
+mod aligments_pos;
+use aligments_pos::AlignmentPos;
+mod utils;
+use utils::*;
 #[derive(Parser, Debug)]
 #[command(name = "excord-LR")]
 #[command(author = "Xinchang Zheng <zhengxc93@gmail.com>")]
@@ -52,167 +55,6 @@ struct Cli {
     out: PathBuf,
 }
 
-#[derive(Debug)]
-struct AlignmentPos {
-    chrom: String,
-    start: i64,
-    end: i64,
-    cigar_map: HashMap<char, u32>,
-    strand: i32, // true = forward
-    mapq: u8,
-    raw_cigar: String,
-}
-
-impl AlignmentPos {
-    fn new(
-        chrom: &str,
-        start: &i64,
-        cigar_map: HashMap<char, u32>,
-        strand: &i32,
-        mapq: &u8,
-        cigar_string: &str,
-    ) -> AlignmentPos {
-        let end =
-            start + (*cigar_map.get(&'D').unwrap()) as i64 + (*cigar_map.get(&'M').unwrap()) as i64
-                - 1i64;
-        let chrom_clean: String;
-        if chrom.starts_with(&"chr".to_string()) {
-            chrom_clean = chrom.strip_prefix(&"chr".to_string()).unwrap().to_string();
-        } else {
-            chrom_clean = chrom.to_string();
-        }
-
-        AlignmentPos {
-            chrom: String::from(chrom_clean),
-            start: *start,
-            end: end + 1,
-            cigar_map: cigar_map,
-            strand: *strand,
-            mapq: *mapq,
-            raw_cigar: cigar_string.to_string(),
-        }
-    }
-}
-
-/// # get the position of the first match base.
-fn find_first_match_pos(cigar_str: &str) -> i64 {
-    let mut p = 0i64;
-    let mut p_string = "".to_string();
-    for c in cigar_str.chars() {
-        if c != 'D'
-            && c != 'M'
-            && c != 'I'
-            && c != 'H'
-            && c != 'S'
-            && c != 'P'
-            && c != 'X'
-            && c != '='
-            && c != 'N'
-        {
-            p_string += &c.to_string();
-        } else {
-            if c == 'M' {
-                break;
-            } else {
-                p += p_string.parse::<i64>().unwrap();
-                p_string = "".to_string();
-            }
-        }
-    }
-    // dbg!(cigar_str, p);
-    return p;
-}
-
-/// # Compare two Alignment
-///
-/// From brentp:
-///
-/// ```
-/// output splitters. Splitters are ordered by their offset into the read.
-/// given, cigars of:
-/// A:20S30M100S
-/// B:50S30M50S
-/// C:90S30M30S
-/// we would order them as they are listed. We would output bedpe intervals
-/// for A-B, and B-C
-/// ```
-fn splitter_order_cmp(a: &AlignmentPos, b: &AlignmentPos) -> Ordering {
-    let a_first_match_pos = find_first_match_pos(&a.raw_cigar);
-    let b_first_match_pos = find_first_match_pos(&b.raw_cigar);
-    a_first_match_pos.cmp(&b_first_match_pos)
-}
-
-fn alignment_pos_cmp(a: &AlignmentPos, b: &AlignmentPos) -> Ordering {
-    if a.chrom.cmp(&b.chrom) != Ordering::Equal {
-        a.chrom.as_bytes().cmp(&b.chrom.as_bytes())
-    } else {
-        a.start.cmp(&b.start)
-    }
-}
-
-fn parse_cigar(cigar_str: &str) -> HashMap<char, u32> {
-    let _cigar_str = cigar_str.to_owned();
-
-    let mut n_str = String::new();
-    let mut cigar_map: HashMap<char, u32> = HashMap::from([
-        ('D', 0),
-        ('M', 0),
-        ('I', 0),
-        ('H', 0),
-        ('S', 0),
-        ('P', 0),
-        ('X', 0),
-        ('=', 0),
-        ('N', 0),
-    ]);
-
-    for x in _cigar_str.chars() {
-        if x.is_numeric() {
-            n_str += &x.to_string();
-        } else {
-            // println!("{}",n_str);
-            let n: u32 = n_str.parse::<u32>().unwrap();
-            let previous_n = cigar_map.entry(x).or_insert(0);
-            *previous_n += n;
-            n_str = "".to_string();
-        }
-    }
-
-    cigar_map
-}
-
-fn parse_supplementary_alignment(s: &str) -> AlignmentPos {
-    let sa_vec: Vec<&str> = s.split(',').collect();
-    let chrom = sa_vec[0];
-    let pos = sa_vec[1].parse::<i64>().unwrap();
-    let strand = match sa_vec[2] {
-        "+" => Ok(1),
-        "-" => Ok(-1),
-        _ => Err("No strand info"),
-    };
-    let cigar_str = parse_cigar(sa_vec[3]);
-    let mapq = sa_vec[4].parse::<u8>().unwrap();
-    let _nm = sa_vec[5].parse::<i64>().unwrap();
-    AlignmentPos::new(
-        chrom,
-        &(pos - 1),
-        cigar_str,
-        &strand.unwrap(),
-        &mapq,
-        &sa_vec[3],
-    )
-}
-
-fn absolute_path(path: impl AsRef<Path>) -> io::Result<PathBuf> {
-    let path = path.as_ref();
-    let absolute_path = if path.is_absolute() {
-        path.to_path_buf()
-    } else {
-        env::current_dir()?.join(path)
-    };
-
-    Ok(absolute_path)
-}
 
 fn main() {
     let cli = Cli::parse();
@@ -477,7 +319,7 @@ fn main() {
                     let m = n as i64;
                     if m >= cli.indelMin {
                         let pt = format!("{}\t{}\t{}\t{}\n", chrom_clean, pos, (pos + m), strand);
-                        f.write(pt.as_bytes());
+                        f.write(pt.as_bytes()).unwrap();
                         pos += strand * m; //finally add the del length;
                     } else {
                         pos += strand * m;
@@ -487,7 +329,7 @@ fn main() {
                     let m = n as i64;
                     if m >= cli.indelMin {
                         let pt = format!("{}\t{}\t{}\t{}\n", chrom_clean, pos, (pos + m), strand);
-                        f.write(pt.as_bytes());
+                        f.write(pt.as_bytes()).unwrap();
                         pos += strand * m; //finally add the del length;
                     } else {
                         pos += strand * m;
